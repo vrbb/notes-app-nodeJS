ffe2454754da40f628e8bcdfdab1905398d09196
victoria <victoriaricartebispo@gmail.com>
2022-05-29 11:42:29 -0300

    Arrow functions

--- playground/1-json.js
+++ playground/1-json.js
@@ -3,6 +3,6 @@ const fs = require('fs')
 const jsonFile = fs.readFileSync('notes.json')
 
 const jsonFileData = JSON.parse(jsonFile)
-console.log (jsonFile,jsonFileData)
+console.log(jsonFile, jsonFileData)
 jsonFileData.name = 'Anderson'
-console.log (jsonFile,jsonFileData)
+console.log(jsonFile, jsonFileData)
