d21b660371c0e4349768f959fbec3a3344d87e3d
victoria <victoriaricartebispo@gmail.com>
2022-05-29 11:52:20 -0300

    Arrow Functions

--- playground/2-arrow-functions.js
+++ playground/2-arrow-functions.js
@@ -6,6 +6,35 @@
 //     return x * x
 // }
 
-const square = (x) => x * x
+// const square = (x) => x * x
 
-console.log(square(3))
+// console.log(square(3))
+
+
+var event = {
+    name : 'Birthday party',
+    guestList : ['Anderson', 'Tidinha', 'Felipe'],
+    printGuestList : function () {
+        const that = this
+        console.log('Guest list for '+this.name)
+        this.guestList.forEach(function (guest) {
+            console.log(guest + ' is attendingo ' + that.name)
+        });
+    }
+}
+
+event.printGuestList()
+event = {
+    name : 'Birthday party',
+    // printGuestList :  () => console.log('Guest list for '+this.name)
+    guestList : ['Anderson', 'Tidinha', 'Felipe'],
+    printGuestList : function () {
+        
+        console.log('Guest list for '+this.name)
+        this.guestList.forEach( (guest) =>{
+            console.log(guest + ' is attendingo ' + this.name)
+        });
+    }
+}
+
+event.printGuestList()
